57be8211fa3018db9417c0af837846fcc1a24b36
SouBeatrizKaroline <111443798+SouBeatrizKaroline@users.noreply.github.com>
2025-06-21 08:47:07 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Usar as rotas definidas
+app.use('/api', routes);
+
+// Iniciar o servidor
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
